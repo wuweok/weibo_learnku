077a64a424ebfb03fb447ad6c698f59fe3fd6694
wuweok <wuweigang163@163.com>
2019-05-19 21:27:40 +0800

    add

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -41,19 +41,21 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-       $credentials = $this->validate($request, [
-           'email' => 'required|email|max:255',
-           'password' => 'required'
-       ]);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
 
-       if (Auth::attempt($credentials, $request->has('remember'))) {
-           session()->flash('success', '欢迎回来！');
-           $fallback = route('users.show', Auth::user());
-           return redirect()->intended($fallback);
-       } else {
-           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-           return redirect()->back()->withInput();
-       }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 
 
