b7a08de9fbadf0d7247fbbe9a2d7a81ac2a90a00
wuweok <wuweigang163@163.com>
2019-05-20 21:31:29 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,13 @@ class User extends Authenticatable
         static::creating(function ($user) {
             $user->activation_token = Str::random(10);
         });
-    }   
+    }  
+
+
+     public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }    
 
     
 }
